5f2f6fa275f7262447a8cc7095ed268a9e42d40e
ReubensIdeas <rfecollege@hotmail.co.uk>
2022-08-22 15:16:57 +0000

    JS creating and gather elements

---
+++ script.js
@@ -0,0 +1,8 @@
+let questionContainer = document.getElementById('questionContainer');
+let questionElement = document.getElementById('question');
+let startButton = document.getElementById('startBtn');
+let nextButton = document.getElementById('nextBtn');
+let answerButton = document.getElementById('answerButtons');
+
+let shuffledQuestions, currentQuestionIndex;
+
