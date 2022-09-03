9e4f73b8d43982f6a4611acb83d421c14a3cfc09
ReubensIdeas <rfecollege@hotmail.co.uk>
2022-09-03 10:35:43 +0000

    moved my questions array into sepereate folder

---
+++ assets/js/questions.js
@@ -0,0 +1,92 @@
+let questions = [
+    {
+        question: 'How long is an elephant pregnant before she gives birth?',
+        answers: [
+            {text: '22 months', correct: true},
+            {text: '7 months', correct: false},
+            {text: '9 months', correct: false},
+            {text: '14 months', correct: false}
+        ]
+    },
+    {
+        question: 'Which mammal is known to have the most powerful bite in the world?',
+        answers: [
+            {text: 'Crocodile', correct: false},
+            {text: 'Hippopotamus', correct: true},
+            {text: 'Lion', correct: false},
+            {text: 'Rhino', correct: false}
+        ]
+    },
+    {
+        question: 'What is the size of a newborn kangaroo?',
+        answers: [
+            {text: '12 inches', correct: false},
+            {text: '6 inches', correct: false},
+            {text: '24 inches', correct: false},
+            {text: '1 inch', correct: true}
+        ]
+    },
+    {
+        question: 'Which animal is known to spend 90% of its day sleeping?',
+        answers: [
+            {text: 'Sloths', correct: false},
+            {text: 'Koalas', correct: true},
+            {text: 'Chimpanzees', correct: false},
+            {text: 'Tigers', correct: false}
+        ]
+    },
+    {
+        question: "What color is a giraffe's tongue?",
+        answers: [
+            {text: 'Brown', correct: false},
+            {text: 'Pink', correct: false},
+            {text: 'Purple', correct: true},
+            {text: 'Red', correct: false}
+        ]
+    },
+    {
+        question: "Under their fur, what color is a polar bear's skin?",
+        answers: [
+            {text: 'Black', correct: true},
+            {text: 'Grey', correct: false},
+            {text: 'Pink', correct: false},
+            {text: 'Orange', correct: false}
+        ]
+    },
+    {
+        question: "How long does it take a sloth to digest a meal?",
+        answers: [
+            {text: '8 hours', correct: false},
+            {text: '2 days', correct: false},
+            {text: '2 weeks', correct: true},
+            {text: '3 weeks', correct: false}
+        ]
+    },
+    {
+        question: "What is the only land mammal that can't jump?",
+        answers: [
+            {text: 'Rhino', correct: false},
+            {text: 'Giraffe', correct: false},
+            {text: 'Panda', correct: false},
+            {text: 'Elephant', correct: true}
+        ]
+    },
+    {
+        question: "What is a group of frogs called?",
+        answers: [
+            {text: 'Army', correct: false},
+            {text: 'Colony', correct: false},
+            {text: 'Knot', correct: false},
+            {text: 'All of the above', correct: true}
+        ]
+    },
+    {
+        question: "How many eyes does a caterpillar have?",
+        answers: [
+            {text: 'Twelve', correct: true},
+            {text: 'Six', correct: false},
+            {text: 'Two', correct: false},
+            {text: 'One', correct: false}
+        ]
+    }
+]
